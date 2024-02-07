39e8284d0529550745a28be74fd41d7b0fc0e797
VinayJojare <jojarevinay10@gmail.com>
2024-02-07 16:27:37 +0530

    ignore

--- coding_problem_service/settings.py
+++ coding_problem_service/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path,os
-from .gmail_pass import gmail_passkey
+from .gmail_passkey import gmail_passkey
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
